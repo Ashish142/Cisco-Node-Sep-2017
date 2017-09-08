4e0c974a585028c51224c96826405cec1238f7d2
Magesh <tkmagesh77@gmail.com>
2017-09-08 11:51:24 +0530

    Task Manager - routes and views

--- 11-task-manager/app.js
+++ 11-task-manager/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var calculatorRoutes = require('./routes/calculatorRoutes');
+var taskRoutes = require('./routes/taskRoutes');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/calculator', calculatorRoutes);
+app.use('/tasks', taskRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- 11-task-manager/routes/calculatorRoutes.js
+++ 11-task-manager/routes/calculatorRoutes.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var calculator = require('../services/calculator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a form for calculator');
-  res.render('calculator');
+  var viewData = {
+		n1 : 0,
+		n2 : 0,
+		op : '',
+		result : ''
+	};
+  res.render('calculator', viewData);
 });
 
+router.post('/', function(req, res, next){
+	var n1 = parseInt(req.body.n1, 10),
+		n2 = parseInt(req.body.n2, 10),
+		op = req.body.op;
+	var result = calculator[op](n1, n2);
+	var viewData = {
+		n1 : n1,
+		n2 : n2,
+		op : op,
+		result : result
+	};
+	res.render('calculator', viewData);
+
+})
 module.exports = router;

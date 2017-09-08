e939f84b1ba9701d4210d9a5ae2597c9b4feaecd
Magesh <tkmagesh77@gmail.com>
2017-09-08 12:35:28 +0530

    Task Manager - complete

--- 11-task-manager/routes/taskRoutes.js
+++ 11-task-manager/routes/taskRoutes.js
@@ -1,13 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
+var taskList = [
+	{id : 1, name : 'Watch a movie', isCompleted : false},
+	{id : 2, name : 'Fix that bug', isCompleted : true}
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('tasks/index');
+  var completedCount = taskList.reduce(function(result, task){
+  	return task.isCompleted ? ++result : result;
+  },0);
+  var viewData = {
+  	list : taskList,
+  	completedCount : completedCount
+  };
+  res.render('tasks/index', viewData);
 });
 
 router.get('/new', function(req, res, next){
 	res.render('tasks/new');
-})
+});
 
+router.post('/new', function(req, res, next){
+	var newTaskId = taskList.reduce(function(result, task){
+		return result > task.id ? result : task.id;
+	}) + 1;
+	var newTask = {
+		id : newTaskId,
+		name : req.body.taskName,
+		isCompleted : false
+	};
+	taskList.push(newTask);
+	res.redirect('/tasks');
+});
+
+router.get('/toggle/:id', function(req, res, next){
+	var taskToToggle = taskList.filter(function(task){
+		return task.id === parseInt(req.params.id);
+	})[0];
+	if (taskToToggle){
+		taskToToggle.isCompleted = !taskToToggle.isCompleted;
+	};
+	res.redirect('/tasks');
+});
+
+router.post('/remove', function(req, res, next){
+	taskList = taskList.filter(function(task){
+		return !task.isCompleted;
+	});
+	res.redirect('/tasks');
+})
 module.exports = router;
